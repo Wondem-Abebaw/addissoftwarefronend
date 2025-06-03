9e24d693dc4357b9b2d982f1999f6eed0a0773ab
Wondem Abebaw <wondem5060@gmail.com>
2025-06-04 00:24:52 +0300

    types added

---
+++ src/types/statsType.ts
@@ -0,0 +1,15 @@
+export interface Statistics {
+  totalSongs: number;
+  totalArtists: number;
+  totalAlbums: number;
+  totalGenres: number;
+  songsPerGenre: { _id: string; count: number }[];
+  songsAndAlbumsPerArtist: { artist: string; songs: number; albums: number }[];
+  songsPerAlbum: { artist: string; album: string; songs: number }[];
+}
+
+export interface StatsState {
+  statistics: Statistics | null;
+  loading: boolean;
+  error: string | null;
+}
